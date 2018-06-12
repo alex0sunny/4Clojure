9ffc5f8e591d15e313372b11cff5198ec1bbd821
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2018-06-12 12:01:40 +0300

    Create 112_Sequs_Horribilis.clj

---
+++ 112_Sequs_Horribilis.clj
@@ -0,0 +1,8 @@
+; Create a function which takes an integer and a nested collection of integers as arguments. Analyze the elements of the input collection and return a sequence which maintains the nested structure, and which includes all elements starting from the head whose sum is less than or equal to the input integer.
+
+(fn fs [n [f & r :as s]]
+  (if f 
+      (if (sequential? f) 
+          (list (fs n f))
+          (if (< n f) '() (cons f (fs (- n f) r))))
+      '()))
