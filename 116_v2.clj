1967dcf975f2bfa77f3f8c0ac63214d7fed91b38
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2018-06-22 14:38:54 +0300

    Update and rename 116_v2.clj to 116_Prime Sandwich.clj

--- 116_v2.clj
+++
@@ -1,5 +0,0 @@
-(fn fs [n] 
-  (let [fp #(every? (comp pos? (partial mod %)) (range 2 %))
-        c (some #(and (fp %) %) (drop (inc n) (range)))
-        [b a] (filter fp (range n 1 -1))]
-     (and (> n 2) (= n b) (= (- n a) (- c n)))))
