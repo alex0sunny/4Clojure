44ed33e90dd703c6bd4644f9d0203e6857f3bbbc
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2018-08-09 17:00:30 +0300

    Update 124_Analyze Reversi.clj

--- 124_Analyze Reversi.clj
+++ 124_Analyze Reversi.clj
@@ -4,9 +4,7 @@
            [e e b e]])
 (def wb 'w)
 (def bw (if (= wb 'w) 'b 'w))
-(def combs #{[wb bw wb] [wb bw bw wb]})
 (def rg (range 4))
-(def rg-s (set rg))
 (def f-ds-prs #(map (partial apply (partial map list))
                     [[rg %] [rg (rest %)] [(rest rg) %]]))
 (def ds-prs (mapcat f-ds-prs [rg (reverse rg)]))
@@ -14,15 +12,31 @@
 (def rs-prs (map (partial map reverse) cs-prs))
 (def ls-prs (concat rs-prs cs-prs ds-prs))
 (defn f-line [brd prs] (hash-map :prs prs :dat (map (partial get-in brd) prs)))
+
+(def f-line #(hash-map :prs %2 :dat (map (partial get-in %) %2)))
+
 (defn f-lines [brd] (map (partial f-line brd) ls-prs))
+
+(def f-lines #(map (partial f-line %) ls-prs))
+
 (def prs (filter #(= 'e (get-in brd %)) (mapcat #(map (partial list %) rg) rg)))
+
+;;(defn f-ck-lm [{prs :prs dat :dat}] 
+;;	(let [[f] (filter #(#{[wb bw wb] [wb bw bw wb]} (% dat))
+;;	                  [identity rest butlast])] 
+;;      (if f (butlast (rest (f prs))))))
+
+;; f-cl-lm probably the core function of the solution. Take the line map -- this map has :prs and :dat keys. The :prs key correcopnds to indicies pairs for each cell of the line and the :dat key corresponds to the letters in line cells.
 (defn f-ck-lm [{prs :prs dat :dat}] 
-	(let [f (some #(if (#{[wb bw wb] [wb bw bw wb]} (% dat)) %)
-	              [identity rest butlast])] 
-      	     (if f (butlast (rest (f prs))))))
-(defn f-ck-move [pair] 
-  (let [ls-maps (f-lines (assoc-in brd pair wb))] (mapcat f-ck-lm ls-maps)))
+     (#(if % (butlast (rest (% prs))))
+       (first (filter #(#{[wb bw wb] [wb bw bw wb]} (% dat)) 
+                       [identity rest butlast]))))
+
+(defn f-ck-move [pair] (mapcat f-ck-lm (f-lines (assoc-in brd pair wb))))
+
 (def res (reduce (fn [m p] (#(if (empty? %) m (assoc m p (set %))) 
                              (f-ck-move p))) {} prs))
 
 (println prs)
+(println (f-ck-move (nth prs 3)))
+(println res)
