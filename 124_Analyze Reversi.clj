8fedc376106b9d3d91ad11226bf2a003fecfb391
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2018-08-09 18:33:09 +0300

    Update 124_Analyze Reversi.clj

--- 124_Analyze Reversi.clj
+++ 124_Analyze Reversi.clj
@@ -2,6 +2,25 @@
            [e w b e]
            [w w b e]
            [e e b e]])
+
+(defn f-reversi [brd wb]
+  (let [bw (if (= wb 'w) 'b 'w) 
+        rg (range 4)
+        f-ds-prs #(map (partial apply (partial map list))
+                       [[rg %] [rg (rest %)] [(rest rg) %]])
+        cs-prs (map #(map list rg (repeat %)) rg)
+        ls-prs (concat cs-prs (map (partial map reverse) cs-prs)
+                       (f-ds-prs rg) (f-ds-prs (reverse rg)))
+        combs-prs (mapcat #(list % (rest %) (butlast %)) ls-prs)
+        f-line #(hash-map :prs %2 :dat (map (partial get-in %) %2))
+        f-lines #(map (partial f-line %) combs-prs)
+        prs (filter #(= 'e (get-in brd %)) (mapcat #(map (partial list %) rg) rg))
+        f-ck-lm (fn [{prs :prs dat :dat}] 
+                  (if (#{[wb bw wb] [wb bw bw wb]} dat) (butlast (rest prs))))
+        f-ck-move #(mapcat f-ck-lm (f-lines (assoc-in brd % wb)))]
+    (reduce (fn [m p] (#(if (empty? %) m (assoc m p (set %))) (f-ck-move p))) 
+            {} prs))
+
 (def wb 'w)
 (def bw (if (= wb 'w) 'b 'w))
 (def rg (range 4))
