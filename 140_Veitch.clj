0bf6227acf68aef1e7883cfc7db5f3ff10638821
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-04-05 18:51:26 +0300

    superset alias

--- 140_Veitch.clj
+++ 140_Veitch.clj
@@ -4,8 +4,7 @@
         fl (fn [sym] (symbol (clojure.string/lower-case (str sym))))
         ftr (fn [si so] (#(if (and (= 2 (count %)) (= 1 (count (set (map fl %))))) 
                               [so (apply disj si %)]) (D (U si so) (I si so))))
-        fwr (fn [si ss] (#(if (some (partial clojure.set/superset? si) %) % ss)
-                          (disj ss si)))
+        fwr (fn [si ss] (#(if (some (partial SU si) %) % ss) (disj ss si)))
         fck (fn [si ss] (#(if (not= % ss) %) 
                               (let [sst (set (mapcat #(ftr si %) ss))]
                                 (if-not (empty? sst) (U (disj ss si) sst) (fwr si ss)))))
