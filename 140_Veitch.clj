a7c771dd70589d5cc8e66915df2f1f48cbb4d051
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-04-12 20:33:59 +0300

    Update 140_Veitch.clj

--- 140_Veitch.clj
+++ 140_Veitch.clj
@@ -1,14 +1,10 @@
-(defn fkarn [in] 
-  (let [[D U I SU] [clojure.set/difference clojure.set/union
-                    clojure.set/intersection clojure.set/superset?]
-        fl (fn [sym] (symbol (clojure.string/lower-case (str sym))))
-        ftr (fn [si so] (#(if (and (= 2 (count %)) (= 1 (count (set (map fl %))))) 
-                              [so (apply disj si %)]) (D (U si so) (I si so))))
-        fwr (fn [si ss] (#(if (some (partial SU si) %) % ss) (disj ss si)))
-        fck (fn [si ss] (#(if (not= % ss) %) 
-                              (let [sst (set (mapcat #(ftr si %) ss))]
-                                (if-not (empty? sst) (U (disj ss si) sst) (fwr si ss)))))
-        ssr ((fn fre [ss] (if-let [ssn (some #(fck % ss) ss)] (fre ssn) ss)) in)
-        fcp (fn [ss] (every? (fn [s] (some #(SU s %) ss)) in))
-        ffin (fn [ss] (some #(if (fcp %) %) (map (partial disj ss) ss)))]
-    (last (take-while identity (iterate ffin ssr)))))
+(fn fkarn [in] 
+  (let [fl (fn [ss] (set (map #(symbol (.toLowerCase (str %))) ss)))
+        ftr (fn [si so] (#(if (= 1 (count (fl %))) (apply disj si %)) 
+                          (into (apply disj si so) (apply disj so si))))
+        fck (fn [ss si] (#(if (not-empty %) (into (disj ss si) %) ss) 
+                          (set (filter identity (map (partial ftr si) ss)))))
+        fte #(last (take-while identity (iterate % %2)))
+        ssr (fte (fn [ss] (#(if (not= ss %) %) (reduce fck ss ss))) in)
+        fcp (fn [ss] (every? (fn [s] (some #(empty? (apply disj % s)) ss)) in))] 
+    (fte (fn [ss] (some #(if (fcp %) %) (map (partial disj ss) ss))) ssr)))
