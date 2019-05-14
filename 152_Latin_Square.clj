1718f4b6660e76529a003ca89776db2e175083b8
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-05-14 13:36:42 +0300

    working solution

--- 152_Latin_Square.clj
+++ 152_Latin_Square.clj
@@ -5,17 +5,17 @@
                           (#(list % (reverse %)) (fw r))))
         fre (fn [vs rs] (mapcat (fn [r] (map #(concat % [r]) vs)) rs))
         als (#(reduce fre (map list (first %)) (rest %)) (map fr v))
-        frecs 
-          (fn [al]
-            (set
-              (filter
-                #(apply = (map sort (concat % (apply map list %))))
-                  (mapcat identity
-                    (for [i (butlast (range w)) j (butlast (range l))
-                          :when (nth (nth al i) j)]
-                      (for [le (range 1 (min (- l j) (- w i)))
-                            :let [rec (map #(map (partial nth (nth al %)) 
-                                                 (range j (+ j le 1))) 
-                                           (range i (+ i le 1)))]
-                            :while (every? identity (flatten rec))] rec))))))]
+        frecs (fn [al]
+                (set
+                  (filter #(apply = (map sort (concat % (apply map list %))))
+                    (mapcat identity
+                      (for [i (butlast (range w)) j (butlast (range l))
+                            :when (nth (nth al i) j) 
+                            :let [le (min (- l j) (- w i))
+                                  rec (map #(map (partial nth (nth al %)) 
+                                                 (range j (+ j le))) 
+                                           (range i (+ i le)))]]
+                        (take-while #(> (count %) 1) 
+                          (drop-while #(some nil? (flatten %))
+                            (iterate #(butlast (map butlast %)) rec))))))))]
     (frequencies (map count (reduce into #{} (map frecs als))))))
