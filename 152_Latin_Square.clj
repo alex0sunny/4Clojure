72021d4fa8a275a8ba9fb0a16a01b09d20ce5644
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-05-14 14:34:38 +0300

    remove avoidable 'set'

--- 152_Latin_Square.clj
+++ 152_Latin_Square.clj
@@ -6,16 +6,15 @@
         fre (fn [vs rs] (mapcat (fn [r] (map #(concat % [r]) vs)) rs))
         als (#(reduce fre (map list (first %)) (rest %)) (map fr v))
         frecs (fn [al]
-                (set
-                  (filter #(apply = (map sort (concat % (apply map list %))))
-                    (mapcat identity
-                      (for [i (butlast (range w)) j (butlast (range l))
-                            :when (nth (nth al i) j) 
-                            :let [le (min (- l j) (- w i))
-                                  rec (map #(map (partial nth (nth al %)) 
-                                                 (range j (+ j le))) 
-                                           (range i (+ i le)))]]
-                        (take-while #(> (count %) 1) 
-                          (drop-while #(some nil? (flatten %))
-                            (iterate #(butlast (map butlast %)) rec))))))))]
+                (filter #(apply = (map sort (concat % (apply map list %))))
+                  (mapcat identity
+                    (for [i (butlast (range w)) j (butlast (range l))
+                          :when (nth (nth al i) j) 
+                          :let [le (min (- l j) (- w i))
+                                rec (map #(map (partial nth (nth al %)) 
+                                               (range j (+ j le))) 
+                                         (range i (+ i le)))]]
+                      (take-while #(> (count %) 1) 
+                        (drop-while #(some nil? (flatten %))
+                          (iterate #(butlast (map butlast %)) rec)))))))]
     (frequencies (map count (reduce into #{} (map frecs als))))))
