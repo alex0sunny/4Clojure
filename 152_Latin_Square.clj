0979d8aa0ec453778d82a4b43d455c41269a074a
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-05-17 16:26:32 +0300

    remove for modifiers

--- 152_Latin_Square.clj
+++ 152_Latin_Square.clj
@@ -1,21 +1,19 @@
 (fn [v]
   (let [l (apply max (map count v)) w (count v)
         fw (fn [r] (map #(take % (repeat nil)) (range (- (inc l) (count r)))))
-        fr (fn [r] (apply (partial map #(vec (concat % r %2))) 
-                          (#(list % (reverse %)) (fw r))))
-        fre (fn [vs rs] (mapcat (fn [r] (map #(vec (concat % [r])) vs)) rs))
+        fr (fn [r] 
+             (apply (partial map #(concat % r %2)) (#(list % (reverse %)) (fw r))))
+        fre (fn [vs rs] (mapcat (fn [r] (map #(concat % [r]) vs)) rs))
         als (#(reduce fre (map list (first %)) (rest %)) (map fr v))
-        frecs (fn [al]
+        fsqs (fn [al]
                 (filter #(apply = (map sort (concat % (apply map list %))))
                   (mapcat identity
-                    (for [i (butlast (range w)) j (butlast (range l))
-                          :when (nth (nth al i) j) 
-                          :let [le (min (- l j) (- w i))
-                                rec (map #(map (partial nth (nth al %)) 
-                                               (range j (+ j le))) 
-                                         (range i (+ i le)))]]
+                    (for [i (butlast (range w)) j (butlast (range l))]
                       (take-while #(> (count %) 1) 
                         (drop-while #(some nil? (flatten %))
-                          (iterate #(butlast (map butlast %)) rec)))))))]
-    (frequencies (map count (reduce into #{} (map frecs als))))))
+                          (iterate #(butlast (map butlast %)) 
+                            (#(take % (map (partial take %) 
+                                        (map (partial drop j) (drop i al)))) 
+                             (min (- l j) (- w i))))))))))]
+    (frequencies (map count (reduce into #{} (map fsqs als))))))
 
