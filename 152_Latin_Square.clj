4805da06de5b8dd5cf652ea8beae25552cdb7c97
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-05-28 16:14:05 +0300

    introduce flat

--- 152_Latin_Square.clj
+++ 152_Latin_Square.clj
@@ -4,16 +4,14 @@
                      (map #(concat (take % sn) r (take (- lr %) sn)) (range (inc lr)))))
         fre (fn [vs rs] (mapcat (fn [r] (map #(concat % [r]) vs)) rs))
         als (#(reduce fre (map list (first %)) (rest %)) (map fr v))
-        fsqs 
-          (fn [al]
-            (filter #(apply = (map sort (concat % (apply map list %) [(set (first %))])))
-              (mapcat identity
-                (for [i (butlast (range w)) j (butlast (range l))]
-                  (take-while #(> (count %) 1) 
-                    (drop-while #(some nil? (flatten %))
-                      (iterate #(butlast (map butlast %)) 
-                        ( #(take % (map (partial take %)             
-                                     (map (partial drop j) (drop i al))))
-                          (min (- l j) (- w i)) ))))))))]
+        flat #(apply = (map sort (concat % (apply map list %) [(set (first %))])))
+        fsqs (fn [al] (mapcat #(filter flat %)
+                        (for [i (butlast (range w)) j (butlast (range l))]
+                          (take-while #(> (count %) 1) 
+                            (drop-while #(some nil? (flatten %))
+                              (iterate #(butlast (map butlast %)) 
+                                ( #(take % (map (partial take %)             
+                                             (map (partial drop j) (drop i al))))
+                                  (min (- l j) (- w i)) )))))))]
     (frequencies (map count (reduce into #{} (map fsqs als))))))
 
