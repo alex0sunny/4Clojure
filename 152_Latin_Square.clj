6942f5df6a562db75ae2e7ae8a95854c283ead9d
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-05-30 15:35:48 +0300

    add comments

--- 152_Latin_Square.clj
+++ 152_Latin_Square.clj
@@ -1,3 +1,22 @@
+;; General approach. Represent each alignment as a nil containing rectangle.
+;; Get all possible squares from each alignment without nils.
+;; Then filter Latin squares. From the result sequence make the set of the Latin squares.
+;; From this set with 'count' and 'frequencies' get the required map.
+;; l -- maximum row len
+;; w -- column len for the rectangles to be constructed
+;; fr -- returns all possible nil-wrappings for the original row 'r'
+;; fre -- compliments partially contructed alignments 'vs' with possible next row 
+;;        variants 'rs'
+;; als -- possible alignments
+;; flat -- checks whether the current square is Latin
+;; fsqs -- from a given alignment 'al' returns all contained latin squares.
+;;         For each possible i, j upper-left corner we get all possible squares. To
+;;         avoid timeout error we first get maximum possible square with the set
+;;         upper-left corner and then successively remove last column and last row.
+;;         During this operation we filter out nil containing squares.
+;; last line -- get Latin squares from all alignments into single set, 
+;;              then from that set with 'count' and 'frequencies' get the final result.
+
 (fn [v]
   (let [l (apply max (map count v)) w (count v)
         fr (fn [r] (let [lr (- l (count r)) sn (repeat nil)]
