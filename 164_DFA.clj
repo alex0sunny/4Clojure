b279a17466f9a5e4b120f2985a9690f05b8030e0
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-09-27 19:07:52 +0300

    use short names

--- 164_DFA.clj
+++ 164_DFA.clj
@@ -1,17 +1,11 @@
-(fn fdfa [{states :states alphabet :alphabet start :start
-             accepts :accepts transitions :transitions}] 
-  (let [f-update-map0 
-          (fn [q-prev cur-map prev-map] 
-            (reduce 
-              (fn [cur-map [a q]] 
-                (assoc cur-map q 
-                  (set (concat (get cur-map q)
-                         (map #(concat % [a]) (get prev-map q-prev))))))
-              cur-map (into [] (get transitions q-prev))))
-        f-update-map 
-          (fn [prev-map]
-            (reduce #(f-update-map0 %2 % prev-map) {} (keys prev-map)))
-        maps-seq (take-while #(not= {} %) 
-                   (rest (iterate f-update-map (hash-map start [[]]))))]
-    (map #(apply str %) (mapcat (fn [[q s]] (if (get accepts q) (seq s)))
-                          (mapcat #(into [] %) maps-seq)))))
+(fn [{st :start ac :accepts mt :transitions}] 
+  (let [fup0 (fn [qp mc mp] 
+               (reduce (fn [mc [a q]] 
+                         (assoc mc q (concat (get mc q)
+                                       (map #(concat % [a]) (get mp qp)))))
+                 mc (into [] (get mt qp))))
+        fup (fn [mp] (reduce #(fup0 %2 % mp) {} (keys mp)))
+        sm (take-while #(not= {} %) 
+                   (rest (iterate fup (hash-map st [[]]))))]
+    (map #(apply str %) (mapcat (fn [[q s]] (if (get ac q) s))
+                          (mapcat #(into [] %) sm)))))
