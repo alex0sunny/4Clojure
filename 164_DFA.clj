d8ce86539e5e4c31de21cd5380f0cc31e9516d46
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-09-28 16:39:37 +0300

    seq -> str

--- 164_DFA.clj
+++ 164_DFA.clj
@@ -2,9 +2,8 @@
   (let [fup0 (fn [qp mc mp] 
                (reduce (fn [mc [a q]] 
                          (assoc mc q (concat (get mc q)
-                                       (map #(concat % [a]) (get mp qp)))))
+                                       (map #(str % a) (get mp qp)))))
                  mc (into [] (get mt qp))))
         fup (fn [mp] (reduce #(fup0 %2 % mp) {} (keys mp)))
-        sm (take-while not-empty (rest (iterate fup (hash-map st [[]]))))]
-    (map #(apply str %) (mapcat (fn [[q s]] (if (get ac q) s))
-                          (mapcat seq sm)))))
+        sm (take-while not-empty (rest (iterate fup (hash-map st [""]))))]
+    (mapcat (fn [[q s]] (if (get ac q) s)) (mapcat seq sm))))
