9165176a42505817f53dc788f0645c3185a64ac7
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-09-30 22:45:12 +0300

    into -> seq

--- 164_DFA.clj
+++ 164_DFA.clj
@@ -3,7 +3,7 @@
                (reduce (fn [mc [a q]] 
                          (assoc mc q (concat (q mc)
                                        (map #(str % a) (qp mp)))))
-                 mc (into [] (qp mt))))
+                 mc (seq (qp mt))))
         fup (fn [mp] (reduce #(fup0 %2 % mp) {} (keys mp)))
         sm (take-while not-empty (iterate fup (hash-map st [""])))]
     (mapcat (fn [[q s]] (if (q ac) s)) (mapcat seq sm))))
