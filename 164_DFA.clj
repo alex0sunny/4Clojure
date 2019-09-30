1cf9c8a88a455f9dfabc5b22568f430ddcfcbd58
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-09-30 11:42:18 +0300

    exclude 'get'

--- 164_DFA.clj
+++ 164_DFA.clj
@@ -1,9 +1,9 @@
 (fn [{st :start ac :accepts mt :transitions}] 
   (let [fup0 (fn [qp mc mp] 
                (reduce (fn [mc [a q]] 
-                         (assoc mc q (concat (get mc q)
-                                       (map #(str % a) (get mp qp)))))
-                 mc (into [] (get mt qp))))
+                         (assoc mc q (concat (q mc)
+                                       (map #(str % a) (qp mp)))))
+                 mc (into [] (qp mt))))
         fup (fn [mp] (reduce #(fup0 %2 % mp) {} (keys mp)))
-        sm (take-while not-empty (iterate fup {st [""]}))]
-    (mapcat (fn [[q s]] (if (get ac q) s)) (mapcat seq sm))))
+        sm (take-while not-empty (iterate fup (hash-map st [""])))]
+    (mapcat (fn [[q s]] (if (q ac) s)) (mapcat seq sm))))
