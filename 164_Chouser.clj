816c066009ede23e622a3fa360e01966d76feac4
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-12-02 18:10:39 +0300

    :start destructuring

--- 164_Chouser.clj
+++ 164_Chouser.clj
@@ -1,6 +1,6 @@
 ;; The remake of Chouser's great solution.
-(fn f [{c :accepts t :transitions q :q :as m}]
-  (lazy-seq (if-let [[[p s]] (seq (if q q #{["" (:start m)]}))]
+(fn f [{c :accepts t :transitions q :q r :start :as m}]
+  (lazy-seq (if-let [[[p s]] (seq (if q q #{["" r]}))]
               (let [q (into (disj (set q) [p s])
                         (for [[a n] (t s)] [(str p a) n]))]
                 (#(if (c s) (cons p %) %) (f (assoc m :q q)))))))
