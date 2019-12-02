5ff8cae9aa35a5866516148724f589bb6e09e304
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-12-02 19:08:36 +0300

    remove duplicate q vars

--- 164_Chouser.clj
+++ 164_Chouser.clj
@@ -1,6 +1,6 @@
-;; The remake of Chouser's great solution.
-(fn f [{c :accepts t :transitions q :q r :start :as m}]
-  (lazy-seq (if-let [[[p s]] (seq (if q q #{["" r]}))]
-              (let [q (into (disj (set q) [p s])
+;; The remake of Chouser's solution.
+(fn f [{c :accepts t :transitions u :u r :start :as m}]
+  (lazy-seq (if-let [[[p s]] (seq (if u u #{["" r]}))]
+              (let [q (into (disj (set u) [p s])
                         (for [[a n] (t s)] [(str p a) n]))]
-                (#(if (c s) (cons p %) %) (f (assoc m :q q)))))))
+                (#(if (c s) (cons p %) %) (f (assoc m :u q)))))))
