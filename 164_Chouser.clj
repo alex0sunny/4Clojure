281e4f98df2775c7f0522c4e6bfca6b7990c079c
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-12-02 17:55:09 +0300

    destructuring simplified
    use plain if instead of destructuring

--- 164_Chouser.clj
+++ 164_Chouser.clj
@@ -1,7 +1,6 @@
 ;; The remake of Chouser's great solution.
-(fn f [{c :accepts t :transitions q :q :as m 
-        :or {q #{["" (:start m)]}}}]
-  (lazy-seq (if-let [[[p s]] (seq q)]
-              (let [q (into (disj q [p s])
+(fn f [{c :accepts t :transitions q :q :as m}]
+  (lazy-seq (if-let [[[p s]] (seq (if q q #{["" (:start m)]}))]
+              (let [q (into (disj (set q) [p s])
                         (for [[a n] (t s)] [(str p a) n]))]
                 (#(if (c s) (cons p %) %) (f (assoc m :q q)))))))
