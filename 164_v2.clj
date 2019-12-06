c89977f2f031df3f1b8c1fd674d38396b5efad81
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2019-12-06 12:43:50 +0300

    Update and rename 164_Chouser.clj to 164_v2.clj

--- 164_v2.clj
+++ 164_v2.clj
@@ -1,4 +1,3 @@
-;; The remake of Chouser's solution.
 (fn f [{c :accepts t :transitions u :u r :start :as m}]
   (lazy-seq (if-let [[[p s]] (seq (if u u #{["" r]}))]
               (let [q (into (disj (set u) [p s])
