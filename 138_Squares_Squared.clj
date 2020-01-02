41f8df25714319514aa5d8137c74243039b2458e
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-01-02 23:04:28 +0300

    Create 138_Squares_Squared.clj

---
+++ 138_Squares_Squared.clj
@@ -0,0 +1,19 @@
+(fn [b e]
+  (let [s (apply str (take-while #(<= % e) (iterate #(* % %) b)))
+        [l] (drop-while #(< (* % %) (count s)) (range))
+        els (#(butlast (interleave % %)) (range 1 (inc l)))
+        fro #(vec (apply map vector (map reverse %)))
+        es (map first
+             (rest 
+               (reductions 
+                 (fn [[_ i] el] (split-at el (concat i (repeat "*"))))
+                 [nil s] els)))
+        sq0 (reduce #(cons %2 (fro %)) [[]] es)
+        sq1 (map #(interpose " " %) (if (even? l) (fro (fro sq0)) sq0))
+        lb (dec (+ l l))
+        sq (vec (map vec (interpose (take lb (repeat " ")) sq1)))
+        si (map #(- % (dec l)) [(dec lb) 0])
+        sis (map #(map + [% %] si) (range lb))
+        ds (for [si sis] (map #(or (get-in sq %) " ")
+                           (map #(map + si [(- %) %]) (range lb))))]
+    (vec (map #(apply str %) ds))))
