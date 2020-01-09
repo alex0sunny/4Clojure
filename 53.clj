0316aa121fefe9d208916cc6b4e32b435c5ed37a
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-01-09 14:19:48 +0300

    Create 53.clj

---
+++ 53.clj
@@ -0,0 +1,11 @@
+;; Longest Increasing Sub-Seq
+(fn [[f & e :as s]] 
+  (first 
+    (sort-by count > 
+      (filter #(not= 1 (count %))
+        (reduce 
+          (fn [r e] 
+		    (#(if (> e (last %)) 
+			    (concat (drop-last r) [(concat % [e])]) 
+                (concat r [[e]])) (last r)))
+          [[] [f]] e)))))
