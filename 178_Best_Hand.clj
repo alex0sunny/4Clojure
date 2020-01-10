a10a8ae2e165150564569e5d8def8c4e0a385cc1
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-01-10 16:26:41 +0300

    Update 178_Best_Hand.clj

--- 178_Best_Hand.clj
+++ 178_Best_Hand.clj
@@ -4,12 +4,8 @@
                :rank ((into {} (map vector "23456789TJQKA" (range))) r)})
         ms (map fcs cs)
         rks (map :rank ms)
-        sus (set (map :suit ms))
-        s1 (set rks)
-        fsm (frequencies rks)
-        fs (vals fsm)
+        fs (vals (frequencies rks))
         [pair two-pair] (filter #(= 2 %) fs)
-        s2 (for [[rk f] fsm :when (> f 1)] rk)
         cnt ((fn [rks] (->> (range 2 (inc (count rks)))
                             (mapcat #(partition % 1 (sort rks)))
                             (filter #(apply = (map - % (range))))
@@ -17,11 +13,11 @@
                             (apply max-key count)
                             count)) rks)
         straight (or (= 5 cnt) (= (sort rks) [0 1 2 3 12]))
-        flush (not (second sus))]
-    (cond (and straight flush) :straight-flush
+        flush_ (not (second (set (map :suit ms))))]
+    (cond (and straight flush_) :straight-flush
           ((set fs) 4) :four-of-a-kind
           (= 2 (count fs)) :full-house
-          flush :flush
+          flush_ :flush
           straight :straight
           ((set fs) 3) :three-of-a-kind
           two-pair :two-pair
