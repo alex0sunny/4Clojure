2b416607fecb4b30a9f99d6ecde32071a8fe0dca
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-01-19 11:48:35 +0300

    Update 164_DFA.clj

--- 164_DFA.clj
+++ 164_DFA.clj
@@ -1,8 +1,8 @@
 (fn [{c :accepts t :transitions r :start}]
   ((fn f [q]
      (lazy-seq 
-       (if-let [[[p s] & qu] (seq q)]
-         (let [u (into (set qu)
-                   (for [[a n] (t s)] [(str p a) n]))]
+       (if-let [[[p s] & qr] (seq q)]
+         (let [u (into (set qr)
+                       (for [[a n] (t s)] [(str p a) n]))]
            (#(if (c s) (cons p %) %) (f u)))))) 
    #{["" r]}))
