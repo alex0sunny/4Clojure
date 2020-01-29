2e601ef1eab9992c6d7109eeb0203ad186d1a6f6
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-01-29 18:02:45 +0300

    nil comparison instead of if

--- 177_Balancing_Brackets.clj
+++ 177_Balancing_Brackets.clj
@@ -1,5 +1,5 @@
 (fn fre [no-brackets nested-brackets in]
-  (#(if (= in %) (if (re-seq no-brackets in) true false)
+  (#(if (= in %) (not= nil (re-seq no-brackets in))
         (fre no-brackets nested-brackets %)) 
     (clojure.string/replace in nested-brackets "")))
 #"^[^(){}\[\]]*$"
