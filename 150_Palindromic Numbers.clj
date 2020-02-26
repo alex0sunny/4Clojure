07e11c7adb37d859d4f800df19a116a960721f5f
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-02-26 16:56:25 +0300

    fi for numbers, fm -- for middle part

--- 150_Palindromic Numbers.clj
+++ 150_Palindromic Numbers.clj
@@ -1,17 +1,19 @@
 (fn fnx [n] 
   (let [fd (fn [x] (map #(- (int %) 48) (str x)))
-        fm #(Long. (apply str %))
+        fi #(Long. (apply str %))
         ds (fd n)
         l (count ds)
         lh (quot (dec l) 2)
         m (nth ds lh)
         dh (take lh ds)
-        fi #(if (odd? l) [%] [% %])
-        nn (fm (concat dh (fi m) (reverse dh)))]
-    (#(if (< nn n) (fnx %) (cons nn (lazy-seq (fnx %))))
-      (cond       
-        (apply = 9 (fd nn)) (+ nn 2)
-        (= 9 m)
-          (#(fm (concat % (fi 0) (reverse %)))
-            (fd (inc (fm dh))))
-        :else (fm (concat dh (map inc (fi m)) (reverse dh)))))))
+        fm #(if (odd? l) [%] [% %])
+        nn (fi (concat dh (fm m) (reverse dh)))]
+    (#(if (< nn n) % (cons nn %))
+      (lazy-seq
+        (fnx
+          (cond       
+            (apply = 9 (fd nn)) (+ nn 2)
+            (= 9 m) (#(fi (concat % (fm 0) (reverse %)))
+                      (fd (inc (fi dh))))
+            :else (fi (concat dh (map inc (fm m)) 
+                              (reverse dh)))))))))
