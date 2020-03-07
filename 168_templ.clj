83346bb1869bbbc93d6e3180ac3ae5e49ee75035
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-07 10:55:18 +0300

    Update 168_templ.clj

--- 168_templ.clj
+++ 168_templ.clj
@@ -1,5 +1,4 @@
-(fn [f & rs] 
-  (let [[m n s t] (if rs rs [0 0])
-        ft (fn ft [fu p] (lazy-seq (cons (fu p) (ft fu (inc p)))))
-        f2 ((fn [fs] (partial ft #(fs % n))) #(ft (partial f %) %2))]
-    (if s (take s (map #(take t %) (f2 m))) (f2 m))))
+(fn [fr f & [m n s t]] 
+  (let [f2 ((fn [fs] (partial fr #(fs % (or n 0)))) #(fr (partial f %) %2))]
+    (if s (take s (map #(take t %) (f2 (or m 0)))) (f2 (or m 0)))))
+(fn fr [fu p] (lazy-seq (cons (fu p) (fr fu (inc p)))))
