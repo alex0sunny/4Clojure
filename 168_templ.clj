d784cb35ffcd7ba8880fa0962c34d8c2a6ff450d
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-07 16:46:03 +0300

    introduce ->

--- 168_templ.clj
+++ 168_templ.clj
@@ -1,4 +1,6 @@
 (fn [fr f & [m n s t]] 
-  (let [f2 ((fn [fs] (partial fr #(fs % (or n 0)))) #(fr (partial f %) %2))]
-    (if s (take s (map #(take t %) (f2 (or m 0)))) (f2 (or m 0)))))
+  (-> #(fr (partial f %) %2)
+      ((fn [fs] (partial fr #(fs % (or n 0)))))
+      (#(% (or m 0)))
+      (#(if s (take s (map (partial take t) %)) %))))
 (fn fr [fu p] (lazy-seq (cons (fu p) (fr fu (inc p)))))
