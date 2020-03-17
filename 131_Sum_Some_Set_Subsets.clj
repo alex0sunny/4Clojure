fb2345dd8566116b1dd308deece5986ee81484a3
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-17 11:41:57 +0300

    use informative names

--- 131_Sum_Some_Set_Subsets.clj
+++ 131_Sum_Some_Set_Subsets.clj
@@ -1,3 +1,3 @@
-(fn [fs ff & ss] (apply ff (map fs ss)))
+(fn [fsums ffilt & sets] (apply ffilt (map fsums sets)))
 (fn [s] (reduce #(apply conj % %2 (map (partial + %2) %)) #{} s))
 (fn [f & r] (not= #{} (reduce #(set (filter % %2)) f r)))
