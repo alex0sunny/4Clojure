58c8ccd41e8fa11cd71bc1a712fcf54e8530a7f8
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-17 11:42:57 +0300

    use informative names

--- 131_partial.clj
+++ 131_partial.clj
@@ -1,3 +1,3 @@
-(fn [fs ff & ss] (apply ff (map fs ss)))
+(fn [fsums ffilt & sets] (apply ffilt (map fsums sets)))
 (partial reduce #(apply conj % %2 (map + % (repeat %2))) #{})
 (fn [f & r] (not= #{} (reduce #(set (filter % %2)) f r)))
