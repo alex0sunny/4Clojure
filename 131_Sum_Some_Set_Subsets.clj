8071946158dd4b58beb0e0665ade40eb211995a5
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-17 17:15:27 +0300

    Update 131_Sum_Some_Set_Subsets.clj

--- 131_Sum_Some_Set_Subsets.clj
+++ 131_Sum_Some_Set_Subsets.clj
@@ -1,3 +1,3 @@
-(fn [fsums ffilt & sets] (ffilt (map fsums sets)))
-(fn [s] (reduce #(apply conj % %2 (map (partial + %2) %)) #{} s))
-(fn [ss] (not= [] (reduce #(filter %2 %) ss)))
+(fn [fsums ffilt & sets] (not= [] (ffilt (map fsums sets))))
+(partial reduce #(apply conj % %2 (map + % (repeat %2))) #{})
+(partial reduce #(filter %2 %))
