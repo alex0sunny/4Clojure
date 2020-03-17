e8847a49e4555df5dc992babc49788c5762185b4
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-17 11:00:15 +0300

    conj instead of into

--- 131_Sum_Some_Set_Subsets.clj
+++ 131_Sum_Some_Set_Subsets.clj
@@ -1,3 +1,3 @@
 (fn [fs ff & ss] (apply ff (map fs ss)))
-(fn [s] (reduce #(into (conj % %2) (map + % (repeat %2))) #{} s))
+(fn [s] (reduce #(apply conj % %2 (map (partial + %2) %)) #{} s))
 (fn [f & r] (not= #{} (reduce #(set (filter % %2)) f r)))
