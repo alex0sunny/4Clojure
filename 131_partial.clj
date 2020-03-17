ba20fe26b3ce8b1698eccb23f8edc07a6739f142
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-17 16:46:29 +0300

    Update 131_partial.clj

--- 131_partial.clj
+++ 131_partial.clj
@@ -1,3 +1,3 @@
 (fn [fsums ffilt & sets] (apply ffilt (map fsums sets)))
 (partial reduce #(apply conj % %2 (map + % (repeat %2))) #{})
-(fn [f & r] (not= #{} (reduce #(set (filter % %2)) f r)))
+(fn [& ss] (not= [] (reduce #(filter %2 %) ss)))
