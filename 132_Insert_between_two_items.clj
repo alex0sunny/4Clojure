177cde9c11b30815f873797f5d763acdb99422a6
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-19 12:09:56 +0300

    Update 132_Insert_between_two_items.clj

--- 132_Insert_between_two_items.clj
+++ 132_Insert_between_two_items.clj
@@ -1,2 +1,2 @@
-(fn f [o k & [[a & [b c :as r] :as s]]]
-  (if b (lazy-cat [a] (if (o a b) [k]) (if c (f o k r) [b])) s))
+(fn f [o k & [[a & [b :as r] :as s]]]
+  (if b (lazy-cat [a] (if (o a b) [k]) (f o k r)) s))
