91e395219c88e1645ef4883534146ec2fb584bda
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-24 18:55:07 +0300

    Update 53_thread.clj

--- 53_thread.clj
+++ 53_thread.clj
@@ -1,9 +1,9 @@
-(fn [s]
+(fn f [s]
   (let [fr #(apply < %)
         ff (complement fr)
-        fsp (fn [p] (split-with fr (drop-while ff p)))]
+        fsp (fn [[_ p]] (split-with fr (drop-while ff p)))]
     (->>  
-      (iterate #(fsp (second %)) [[] (partition 2 1 s)])
+      (iterate fsp [[] (partition 2 1 s)])
       (take-while #(not= [[][]] %))
       (#(reverse (map first %)))
       (#(distinct (flatten (apply max-key count %)))))))
