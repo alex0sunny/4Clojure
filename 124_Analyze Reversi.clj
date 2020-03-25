679fda432381030f3e5153274339e4826af8e243
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-03-25 12:05:17 +0300

    Update 124_Analyze Reversi.clj

--- 124_Analyze Reversi.clj
+++ 124_Analyze Reversi.clj
@@ -3,7 +3,7 @@
         rg (range 4)
         r (map #(map list rg (repeat %)) rg) 
         l (apply map list r)
-        d (map #(map list rg %) (mapcat #(list % (reverse %)) 
+        d (map #(map list rg %) (mapcat #(list % (reverse %))
                                         [(range -1 3) rg (range 1 5)]))
         c (mapcat #(list % (rest %) (butlast %)) (concat r l d))
         fw (fn [bd] (mapcat #(rest (butlast %)) 
@@ -11,4 +11,4 @@
                             	      (map (partial get-in bd) %)) c)))
         prs (filter #(= 'e (get-in brd %)) (apply concat r))]
     (reduce (fn [m p] ( #(if (empty? %) m (assoc m p (set %))) 
-                    	   (fw (assoc-in brd p wb)) )) {} prs)))
+                    	  (fw (assoc-in brd p wb)) )) {} prs)))
