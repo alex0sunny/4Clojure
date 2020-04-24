4c82a2bb1e8bc6250ebe365fcb72211ef4a6ce0f
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-04-25 00:11:30 +0300

    Update 148_The_Big_Divide.clj

--- 148_The_Big_Divide.clj
+++ 148_The_Big_Divide.clj
@@ -1,9 +1,3 @@
-;;(fn [n a b] 
-;;  (letfn [(f [n d] (#(/ (* d % (dec %)) 2) (+ 1N (quot (dec n) d))))] 
-;;    (apply #(- (+ % %2) %3) (map #(f n %) [a b (* a b)]))))
-
 (fn [n a b] 
-  (letfn [(f [n d] (#(* 1/2 d % (inc %)) (quot (dec n) d)))] 
-    (apply #(- (+ % %2) %3) (map #(f n %) [a b (* a b)]))))
-
-
+  (#(- (+ (% a) (% b)) (% (* a b)))
+    (fn [d] (#(* 1/2 d % (inc %)) (quot (dec n) d)))))
