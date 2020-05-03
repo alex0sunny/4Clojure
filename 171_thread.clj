a04211fce370edd9f36c13753ec14bfcc775d357
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-05-03 18:50:47 +0300

    Update 171_thread.clj

--- 171_thread.clj
+++ 171_thread.clj
@@ -1,7 +1,6 @@
-  #(if (empty? %) []
-       (->> (sort (set %))
-            ((fn [[f :as s]] (range f (inc (last s)))))
-            (map (set %))
-            (partition-by nil?)
-            (filter first)
-            (map (fn [[f :as s]] [f (last s)]))))
+#(if (empty? %) []
+     (->> (range (apply min %) (inc (apply max %)))
+          (map (set %))
+          (partition-by nil?)
+          (filter first)
+          (map (fn [[f :as s]] [f (last s)]))))
