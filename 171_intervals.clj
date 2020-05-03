7deca870a6fa6d17013208abb686f04e2f959ec2
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-05-03 16:40:49 +0300

    Update 171_intervals.clj

--- 171_intervals.clj
+++ 171_intervals.clj
@@ -1,5 +1,5 @@
 #(if (empty? %) []
-	   (apply reduce 
+     (apply reduce 
               (fn [[[f s] & rs :as r] e]
                 (if (= e (dec f)) (cons [e s] rs) (cons [e e] r)))
               ((fn [[f & rs]] [[[f f]] rs]) (sort > (set %)))))
