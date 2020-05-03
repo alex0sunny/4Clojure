6d912ff4ef4a886150b381424c2878849ae1d1f4
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-05-03 18:32:30 +0300

    Delete 171_v0.clj

--- 171_v0.clj
+++
@@ -1,8 +0,0 @@
-#((fn [[f & rs]]
-    (if-not f []
-      (reverse 
-        (reduce 
-          (fn [[[ff fs] & rs :as r] e] 
-            (if (= (dec e) fs) (cons [ff e] rs) (cons [e e] r)))
-          [[f f]] rs))))
-  (sort (set %)))
