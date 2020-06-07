8b4cbfcb9ebb0b47e850553f4448c30079704652
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-06-07 23:39:43 +0300

    Update 115_The_Balance_of_N.clj

--- 115_The_Balance_of_N.clj
+++ 115_The_Balance_of_N.clj
@@ -1,4 +1,3 @@
 (fn f [n] 
-  (let [s (map #(- (int %) 48) (str n))
-        l (quot (count s) 2)]
+  (let [s (map int (str n)), l (quot (count s) 2)]
     (apply = (map #(apply + (% l s)) [take take-last]))))
