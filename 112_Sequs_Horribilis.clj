f2e77d2abcf60989fea0844872b1253091b141d8
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-06-18 16:22:12 +0300

    remove suspected solution

--- 112_Sequs_Horribilis.clj
+++ 112_Sequs_Horribilis.clj
@@ -1,15 +1,3 @@
-; Create a function which takes an integer and a nested collection of integers as arguments. Analyze the elements of the input collection and return a sequence which maintains the nested structure, and which includes all elements starting from the head whose sum is less than or equal to the input integer.
-
-(fn fs [n [f & r :as s]]
-  (if f 
-      (if (sequential? f) 
-          (#(if (= f %) (cons f (fs (- n (apply + (flatten f))) r))
-                (list %))
-            (fs n f))
-          (if (< n f) '() (cons f (fs (- n f) r))))
-      '()))
-
-; no duplicate summation (apply + (flatten ...))
 #(or (:s (% %2 %3)) [])
 (fn fs [n [f & r :as s]]
   (if f 
