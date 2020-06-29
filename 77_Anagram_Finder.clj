4184f43d039ddf57f50cf6cc9ed861d1f34717c8
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-06-29 15:54:53 +0300

    use thread op

--- 77_Anagram_Finder.clj
+++ 77_Anagram_Finder.clj
@@ -5,4 +5,10 @@
 
 ;; Write a function which finds all the anagrams in a vector of words. A word x is an anagram of word y if all the letters in x can be rearranged in a different order to form y. Your function should return a set of sets, where each sub-set is a group of words which are anagrams of each other. Each sub-set should have at least two words. Words without any anagrams should not be included in the result.
 
-#(set (filter second (map set (vals (group-by frequencies %)))))
+;;#(set (filter second (map set (vals (group-by frequencies %)))))
+
+#( ->> (group-by frequencies %) 
+       vals
+       (map set)
+       (filter second)
+       set )
