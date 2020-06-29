587897d56ac027f67680c3c6fabd468153d263d3
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-06-29 10:42:47 +0300

    Update 112_Sequs_Horribilis.clj

--- 112_Sequs_Horribilis.clj
+++ 112_Sequs_Horribilis.clj
@@ -1,12 +1,15 @@
-#(or (:s (% %2 %3)) [])
-(fn fs [n [f & r :as s]]
-  (if f 
-      (if (sequential? f) 
-          (let [{nn :n f-new :s} (fs n f)]
-            (if f-new
-                {:n nn 
-                 :s (cons f-new 
-                          (if (= f f-new) (:s (fs nn r)))) } ))
-          (let [nn (- n f)]
-              { :n nn 
-		:s (if (>= nn 0) (cons f (:s (fs nn r)))) } ))))
+;; Sequs Horribilis
+;; Difficulty:	Medium
+;; Topics:	seqs
+
+
+;; Create a function which takes an integer and a nested collection of integers as arguments. Analyze the elements of the input collection and return a sequence which maintains the nested structure, and which includes all elements starting from the head whose sum is less than or equal to the input integer.
+
+#(second (% %2 %3))
+ (fn fs [n [f & r]]
+   (if-not f [n []]
+           (if (sequential? f) 
+               (let [[nm fm] (fs n f)]
+                 [nm (cons fm (if (= f fm) (second (fs nm r))))])
+               (let [nm (- n f)]
+                 [nm (if (neg? nm) [] (cons f (second (fs nm r))))]))))
