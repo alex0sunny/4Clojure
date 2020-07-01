ff61348dd4b98acc03af6700e266829af572ee0b
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-07-01 09:54:28 +0300

    Update 150_Palindromic Numbers.clj

--- 150_Palindromic Numbers.clj
+++ 150_Palindromic Numbers.clj
@@ -1,9 +1,20 @@
+;; Palindromic Numbers
+;; Difficulty:	Medium
+;; Topics:	seqs math
+
+
+;; A palindromic number is a number that is the same when written forwards or backwards (e.g., 3, 99, 14341).
+
+;; Write a function which takes an integer n, as its only argument, and returns an increasing lazy sequence of all palindromic numbers that are not less than n.
+
+;; The most simple solution will exceed the time limit!
+
 (fn fnx [n] 
   (let [fd (fn [x] (map #(- (int %) 48) (str x)))
         fi #(Long. (apply str %))
-        l (count (fd n))
-        lp (quot (inc l) 2)
-        p (take lp (fd n))
+        d (fd n)
+        l (count d)
+        p (take (quot (inc l) 2) d)
         fc #(fi (concat 
                   % ((if (odd? l) rest identity) (reverse %))))
         nn (fc p)]
