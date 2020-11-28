0e8b9ce196fc7d02b170a691b914169c599c8a0c
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-11-28 14:26:31 +0300

    center coords calc

--- solutions/138_Squares_Squared.clj
+++ solutions/138_Squares_Squared.clj
@@ -8,15 +8,13 @@
 (fn [b e]
   (let [s (apply str (take-while #(<= % e) (iterate #(* % %) b)))
         [l] (drop-while #(< (* % %) (count s)) (range))
+        ld (dec l)
         xs (->> (map list (#(interleave % %) (rest (range))) 
                           (cycle [[1 1] [1 -1] [-1 -1] [-1 1]]))
                 (mapcat (fn [[ll d]] (take ll (repeat d))))
-                (reductions #(map + % %2) '(0 0))
+                (reductions #(map + % %2) [(- ld (mod ld 2)) ld])
                 (take (* l l)))
         m (into {} (map vector xs (concat s (repeat "*"))))
-        yxs (map #(map % xs) [first second])
-        [[ymi xmi] [yma xma]] (map #(map (partial apply %) yxs)
-                                [min max])]
-    (vec (for [y (range ymi (inc yma))]
-           (apply str (for [x (range xmi (inc xma))] 
-                        (get m [y x] " ")))))))
+        yxs (map #(map % xs) [first second])]
+    (#(vec (for [y %] (apply str (for [x %] (get m [y x] " ")))))
+      (range (+ l ld)))))
