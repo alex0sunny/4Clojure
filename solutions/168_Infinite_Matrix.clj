67d2a1ee47fc5dc8d69c4130200ef4475f61f5c1
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-11-28 11:46:24 +0300

    problem condition added

--- solutions/168_Infinite_Matrix.clj
+++ solutions/168_Infinite_Matrix.clj
@@ -1,3 +1,26 @@
+;; Infinite Matrix
+;; Difficulty:	Medium
+;; Topics:	seqs recursion math
+
+
+;; In what follows, m, n, s, t denote nonnegative integers, f denotes a function that accepts two arguments and is defined for all nonnegative integers in both arguments.
+
+;; In mathematics, the function f can be interpreted as an infinite matrix with infinitely many rows and columns that, when written, looks like an ordinary matrix but its rows and columns cannot be written down completely, so are terminated with ellipses. In Clojure, such infinite matrix can be represented as an infinite lazy sequence of infinite lazy sequences, where the inner sequences represent rows.
+
+;; Write a function that accepts 1, 3 and 5 arguments
+
+;;     with the argument f, it returns the infinite matrix A that has the entry in the i-th row and the j-th column equal to f(i,j) for i,j = 0,1,2,...;
+;;     with the arguments f, m, n, it returns the infinite matrix B that equals the remainder of the matrix A after the removal of the first m rows and the first n columns;
+;;     with the arguments f, m, n, s, t, it returns the finite s-by-t matrix that consists of the first t entries of each of the first s rows of the matrix B or, equivalently, that consists of the first s entries of each of the first t columns of the matrix B.
+
+;; Special Restrictions
+;; for
+;; range
+;; iterate
+;; repeat
+;; cycle
+;; drop
+
 (fn [f & rs] 
   (let [[m n s t] rs 
         [m n] [(or m 0) (or n 0)]
