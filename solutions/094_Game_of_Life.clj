3dfe8fac7433390497ffb06f5fb50ba1349d6124
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-06 00:18:40 +0300

    Update and rename 094.clj to 094_Game_of_Life.clj

--- solutions/094_Game_of_Life.clj
+++ solutions/094_Game_of_Life.clj
@@ -1,4 +1,20 @@
-; Problem 94 Game of Life
+; Game of Life
+; Difficulty:	Hard
+; Topics:	game
+
+
+; The game of life is a cellular automaton devised by mathematician John Conway.
+
+; The 'board' consists of both live (#) and dead ( ) cells. Each cell interacts with its eight
+; neighbours (horizontal, vertical, diagonal), and its next state is dependent on the following rules:
+
+; 1) Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+; 2) Any live cell with two or three live neighbours lives on to the next generation.
+; 3) Any live cell with more than three live neighbours dies, as if by overcrowding.
+; 4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+; Write a function that accepts a board, and returns a board representing the next generation of cells.
+
 (fn [field]
   (let [wrapped-rows (#(concat (list %) field (list %)) (apply str (repeat (count (first field)) " ")))
         wrapped-field (map #(str " " % " ") wrapped-rows)
