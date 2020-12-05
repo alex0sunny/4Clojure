3d5c22203aa1e9e43e9ff1af0f8dd9c98821bd60
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-06 00:16:35 +0300

    Update and rename 082.clj to 082_Word_Chains.clj

--- solutions/082_Word_Chains.clj
+++ solutions/082_Word_Chains.clj
@@ -1,4 +1,17 @@
 ; Problem 82 Word Chains
+; Difficulty:	Hard
+; Topics:	seqs
+
+
+; A word chain consists of a set of words ordered so that each word differs by only one letter from the words 
+; directly before and after it. The one letter difference can be either an insertion, a deletion, 
+; or a substitution. Here is an example word chain:
+
+; cat -> cot -> coat -> oat -> hat -> hot -> hog -> dog
+
+; Write a function which takes a sequence of words, and returns true 
+; if they can be arranged into one continous word chain, and false if they cannot.
+
 (fn [wset] (let [chained? (fn chained? [s1 s2] (or (every? #(>= 1 (count %)) (vector s1 s2))
 								(if (= (first s1) (first s2)) (chained? (rest s1) (rest s2)) 
         	               		(if (= (last s1) (last s2)) (chained? (butlast s1) (butlast s2)) false)) ))
