1a6d12e175a4fd2c066ea1a3fbdec358b8d93f1f
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-06 00:20:39 +0300

    Update 178_Best_Hand.clj

--- solutions/178_Best_Hand.clj
+++ solutions/178_Best_Hand.clj
@@ -1,3 +1,21 @@
+;; Best Hand
+;; Difficulty:	Hard
+;; Topics:	strings game
+
+
+;; Following on from Recognize Playing Cards, determine the best poker hand
+;; that can be made with five cards. The hand rankings are listed below for your convenience.
+
+;;     Straight flush: All cards in the same suit, and in sequence
+;;     Four of a kind: Four of the cards have the same rank
+;;     Full House: Three cards of one rank, the other two of another rank
+;;     Flush: All cards in the same suit
+;;     Straight: All cards in sequence (aces can be high or low, but not both at once)
+;;     Three of a kind: Three of the cards have the same rank
+;;     Two pair: Two pairs of cards have the same rank
+;;     Pair: Two cards have the same rank
+;;     High card: None of the above conditions are met
+
 #(let [rks (map second %)
        fs (sort (vals (frequencies rks)))
        s ((set (map set (partition 5 1 "A23456789TJQKA"))) 
