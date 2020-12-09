10ad8a725a9e48f8c05ce657dae70dff245196e2
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-09 21:29:10 +0300

    Update 106_Number_Maze.clj

--- solutions/106_Number_Maze.clj
+++ solutions/106_Number_Maze.clj
@@ -1,7 +1,22 @@
+;; Difficulty:	Hard
+;; Topics:	numbers
+
+
+;; Given a pair of numbers, the start and end point,
+;; find a path between the two using only three possible operations:
+
+;;     double
+;;     halve (odd numbers cannot be halved)
+;;     add 2
+
+;; Find the shortest path through the "maze". Because there are multiple
+;; shortest paths, you must return the length of the shortest path, not
+;; the path itself.
+
 #(loop [s [%1] c 1]
     (if (some (partial = %2) s) c
       (recur (filter integer?
-         (concat (map (partial * 2)   s) 
-                 (map (partial * 1/2) s) 
-                 (map (partial + 2)   s)))
-         (inc c))))
+                (concat (map (partial * 2)   s) 
+                        (map (partial * 1/2) s) 
+                        (map (partial + 2)   s)))
+             (inc c))))
